ad48461cf5aa30bf05dff9e5de90c101dc75e979
HaniaSalah <mlStudent978@gmail.com>
2024-12-30 04:08:03 +0200

    factorial using recursion

---
+++ main.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+n=int(input("enter number: "))
+
+fact=factorial(n)
+
+print("factorial of",n ,"=", factorial(n))
